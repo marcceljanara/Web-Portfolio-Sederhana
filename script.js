5bf0d2fe39634cb12ac0acac454b824224dbd8d7
I Nengah Marccel <82068856+marcceljanara@users.noreply.github.com>
2023-02-14 21:40:10 +0700

    Mengganti keseluruhan

--- script.js
+++ script.js
@@ -1,29 +1,28 @@
-const navbar = document.querySelector('.js-select');
-const mode = document.querySelector('ul li:nth-child(5)');
-const body = document.querySelector('#home');
-const nav = document.querySelector('nav');
-const jumbotron = document.querySelector('.jumbotron')
-const about = document.getElementById('about');
-const project = document.getElementById('project');
-const h2Project = document.querySelector('.h2-project')
-const card = document.querySelector('.card');
-const wave1 = document.querySelector('svg .wave-1');
-const wave2 = document.querySelector('svg .wave-2');
-const wave3 = document.querySelector('svg .wave-3');
-const wave4 = document.querySelector('svg .wave-4');
-const footer = document.querySelector('footer');
-const contact = document.querySelector('#contact');
-const button = document.querySelector('form button');
+const navbar = document.querySelector(".js-select");
+const mode = document.querySelector(".container .order-3");
+const body = document.querySelector("#home");
+const nav = document.querySelector("nav");
+const jumbotron = document.querySelector(".jumbotron");
+const about = document.getElementById("about");
+const project = document.getElementById("project");
+const h2Project = document.querySelector(".h2-project");
+const card = document.querySelector(".card");
+const wave1 = document.querySelector("svg .wave-1");
+const wave2 = document.querySelector("svg .wave-2");
+const wave3 = document.querySelector("svg .wave-3");
+const wave4 = document.querySelector("svg .wave-4");
+const footer = document.querySelector("footer");
+const contact = document.querySelector("#contact");
+const button = document.querySelector("form button");
 
-const dom = [nav,jumbotron,about,project,h2Project,project,wave1,wave2,wave3,wave4,contact,footer,button];
-const kelas = ['bg-dark','jumbotron-dark','about-dark','project-dark','project-darkh2','project','wave13-dark','wave2-dark','wave13-dark','wave4-dark','contact','bg-dark','btn-dark']
+const dom = [nav, jumbotron, about, project, h2Project, project, wave1, wave2, wave3, wave4, contact, footer, button];
+const kelas = ["bg-dark", "jumbotron-dark", "about-dark", "project-dark", "project-darkh2", "project", "wave13-dark", "wave2-dark", "wave13-dark", "wave4-dark", "contact", "bg-dark", "btn-dark"];
 
-mode.addEventListener('click', () => {
-    for(let i = 0; i< dom.length;i++){
-        dom[i].classList.toggle(kelas[i]);
-    }
-})
-
+mode.addEventListener("click", () => {
+  for (let i = 0; i < dom.length; i++) {
+    dom[i].classList.toggle(kelas[i]);
+  }
+});
 
 // Mengirim email
 
@@ -61,15 +60,17 @@ async function fetchData() {
       response.json();
       const msg = document.querySelector(".msg");
       if (response.ok) {
+        console.log("OK");
         msg.classList.add("success");
         msg.innerHTML = "Success";
         if (msg.classList.contains("fail")) {
           msg.classList.toggle("fail");
         }
-      } else if (response.status) {
+      } else if (response.status === "429") {
         msg.classList.add('fail');
         msg.innerHTML = response.statusText;
       } else {
+        console.log("NOT OK");
         msg.classList.add("fail");
         msg.innerHTML = "Email yang anda masukan salah dan/atau masukan form kosong";
         if (msg.classList.contains("success")) {
